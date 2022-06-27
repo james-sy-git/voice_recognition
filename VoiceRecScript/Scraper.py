625e83ee96ae9d67b38ee8240369da125d928baa
J Sy <jamessy123@yahoo.com>
2022-06-26 23:01:49 -0500

    Implemented partially-working version of Scraper, which uses HTML tags from a Google search page. However, I realized that the results don't match up due to some JavaScript nonsense. Will probably end up using Python googlesearch library.

--- VoiceRecScript/Scraper.py
+++ VoiceRecScript/Scraper.py
@@ -11,11 +11,13 @@ https://www.geeksforgeeks.org/performing-google-search-using-python-code/
 
 https://stackoverflow.com/questions/11513624/python-google-search-scraper-with-beautifulsoup
 
-GO TO THE <A> TAG AND FIND ITS ACCOMPANYING <H3>
+GO TO THE <A> (Hyperlink) TAG AND FIND ITS ACCOMPANYING <H3> (Heading)
+
+need to use list instead of dictionary to maintain Google's result priority and valuation
 """
 
 from bs4 import BeautifulSoup
-from urllib.request import urlopen
+from urllib.request import Request, urlopen
 
 class Scraper:
     '''
@@ -30,6 +32,12 @@ class Scraper:
     
     #attribute _url:
     #invariant" _url is a string in correct https formatting using _query
+
+    def geturl(self):
+        '''
+        Returns this Scraper's URL
+        '''
+        return self._url
 
     def set_query(self, new_query):
         '''
@@ -52,7 +60,8 @@ class Scraper:
         '''
 
         self._query = query # safe get method from class UserListener
-        self._results = {} # or None depending on how we do it
+        self._results = [] # a nested list
+        self._titles = []
         self._url = self.createURL() # url used in search method created using speech to text input by user
 
     def createURL(self):
@@ -73,14 +82,17 @@ class Scraper:
         ex. "Michelle Obama" becomes "Michelle+Obama"
         '''
 
-        pass
+        qu = self._query.replace(' ', '+')
+        return qu
+
 
     def read(self):
         '''
-        reads a url
+        Creates a BeautifulSoup object for this Scraper's URL
         '''
 
-        page = urlopen(self._url)
+        req = Request(self._url, headers={'User-Agent': 'Mozilla/5.0'}) # https://stackoverflow.com/questions/16627227/problem-http-error-403-in-python-3-web-scraping
+        page = urlopen(req)
 
         bytes = page.read()
         html = bytes.decode('utf-8')
@@ -91,14 +103,35 @@ class Scraper:
     def search_urls(self, num_results):
         '''
         Searches google using the search query
-        Returns a dictionary with the title and the URL
+        Returns a nested list with the URL and the title
         '''
 
-        pass
+        cutoff = 0
+
+        page = self.read().body
+
+        all_links = page.find_all('a')
+        for link in all_links:
+            if cutoff < num_results:
+                if link.has_attr('href') and not link.has_attr('class'): # take out the second part if we want the news "fluff"
+                    if 'url?q=' in link.get('href'):
+                        self._results.append(link.get('href'))
+                        try:
+                            title = link.find('h3')
+                            self._titles.append(title.getText())
+                        except:
+                            pass
+                        cutoff = cutoff + 1
 
     def clear(self):
         '''
-        Clears the dictionary
+        Clears the list
         '''
 
-        self._results = {}
+        self._results = []
+
+if __name__ == "__main__":
+    test_query = "obama"
+    test = Scraper(test_query)
+    test.search_urls(4)
+    print(test._titles)
